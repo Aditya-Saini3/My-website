83651d715a5c67ec967e79543d77bd4e217812b7
Aditya Saini <adityasaini2024@gmail.com>
2022-09-06 18:19:48 +0530

    removed unnecessary code

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function changeBg() {
     const header = document.querySelector('header')
     let scrollValue = window.scrollY;
-    console.log(scrollValue);
     if(scrollValue > 50) {
         header.classList.add('sticky')
     } else {
