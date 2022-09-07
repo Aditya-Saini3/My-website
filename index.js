3359c5ab93fafa7c75483916b83302e69d198b83
Aditya Saini <adityasaini2024@gmail.com>
2022-09-07 10:57:46 +0530

    Adding URL to social logos and fixing scroll-padding on navigation to different sections

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const header = document.querySelector('header');
+const headerHeight = header.offsetHeight;
+
+
+document.documentElement.style.setProperty("--_scroll-padding", headerHeight + 20 + "px");
+
+// function to switch bg og navigation
 function changeBg() {
-    const header = document.querySelector('header')
     let scrollValue = window.scrollY;
     if(scrollValue > 50) {
         header.classList.add('sticky')
