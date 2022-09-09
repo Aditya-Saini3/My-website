d817999404fe6c4269a8f3efb9b363ea8be447ca
Aditya Saini <adityasaini2024@gmail.com>
2022-09-09 16:21:55 +0530

    Projects data added

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 const header = document.querySelector('header');
 const headerHeight = header.offsetHeight;
+const mediaTrack = document.querySelector('.media-track');
+
+let dataArray = []
+
+//Function to renderData into .media-track div
+function renderData() {
+    let html = ""
+    for (let dat of dataArray) {
+        html += `
+        <a href=${dat.url} target="_blank">
+        <div class="media-element">
+            <h3>${dat.name}</h3>
+            <img src=${dat.imgPath} />
+        </div>
+        </a>
+        `
+    }
+    mediaTrack.innerHTML = html
+}
+
+//Fetching Data from data.json file
+fetch("./data.json")
+.then(response => {
+   return response.json();
+})
+.then(data => {
+    dataArray = data
+    renderData()
+});
 
 
 document.documentElement.style.setProperty("--_scroll-padding", headerHeight + 20 + "px");
