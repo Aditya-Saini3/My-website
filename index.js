0edd6fe4599553b23af57e477e8e34bf9a7577b9
Aditya Saini <adityasaini2024@gmail.com>
2022-09-12 18:49:26 +0530

    On scroll fade-in and slide-in animations

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const header = document.querySelector('header');
 const headerHeight = header.offsetHeight;
 const mediaTrack = document.querySelector('.media-track');
+const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll(".slide-in");
 
 let dataArray = []
 
@@ -44,5 +46,23 @@ function changeBg() {
     }
 }
 
-
 window.addEventListener('scroll', changeBg);
+
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -250px 0px"
+};
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            return;
+        } else {
+            entry.target.classList.add('appear');
+            appearOnScroll.unobserve(entry.target);
+        }
+    })
+}, appearOptions)
+
+sliders.forEach(slider => {
+    appearOnScroll.observe(slider)
+})
